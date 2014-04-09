24d582dcd77f4b6c5791759993cc17c85e5beeab
Paolo Stivanin <info@paolostivanin.com>
2014-04-09 19:04:28 +0200

    added utility to reverse shellcode

---
+++ utility/for-shellcode/reverse-shellcode.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void){
+	uint8_t shellcode[] = {}; //shellcode in this format: 0x33,0x4f,0x5c,etc
+	int i, j=0;
+	size_t len = sizeof(shellcode);
+	uint8_t t[len];
+	
+	for(i=len-1; i>=0; i--){
+		t[j] = s[i];
+		j++;
+	}
+	
+	printf("Reversed shellcode:\n");
+	for(i=0; i<len; i++){
+		printf("%02x ", t[i]);
+	}
+	printf("\n");
+	
+	return 0;
+}
